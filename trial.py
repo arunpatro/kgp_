dce1116a28c95dc6c7ba6c0bf2621e47324d3b7d
Arun Patro <arun.kumar.patro@gmail.com>
2015-12-01 21:04:56 +0530

    All Electives

--- trial.py
+++ trial.py
@@ -2,16 +2,16 @@ import urllib2
 import requests
 from bs4 import BeautifulSoup
 
-url_UG = 'https://erp.iitkgp.ernet.in/ERPWebServices/curricula/specialisationList.jsp?stuType=UG'
+url_PG = 'https://erp.iitkgp.ernet.in/ERPWebServices/curricula/specialisationList.jsp?stuType=PG'
 url_PG = 'https://erp.iitkgp.ernet.in/ERPWebServices/curricula/specialisationList.jsp?stuType=PG'
 url_dep = 'https://erp.iitkgp.ernet.in/ERPWebServices/curricula/'
 url_download = 'https://erp.iitkgp.ernet.in/ERPWebServices/curricula/commonFileDownloader.jsp?fileFullPath='
 
-db_UG={}
+db_PG={}
 db_PG={}
 db_el={}
 db={}
-ele_UG=[]
+ele_PG=[]
 ele_PG=[]
 
 #to download one file
@@ -19,13 +19,6 @@ def download(url,name):
 	f = urllib2.urlopen(url)
 	with open(name, "wb") as code:
 		code.write(f.read())
-
-#accumulating all UG curriculums for all Departments/Stream
-response_UG = requests.get(url_UG)
-soup_UG = BeautifulSoup(response_UG.text,'html.parser')
-all_dep = soup_UG.find_all('a')
-for link in all_dep:
-	db_UG[link.get('href')[45:]]=url_dep + link.get('href')
 
 #accumulating all PG curriculums for all Departments/Stream
 response_PG = requests.get(url_PG)
@@ -35,19 +28,6 @@ for link in all_dep:
 	db_PG[link.get('href')[45:]]=url_dep + link.get('href')
 
 
-#accumulating all UG courses for each Department
-for item in db_UG:
-	url = db_UG[item]
-	response = requests.get(url)
-	soup = BeautifulSoup(response.text,'html.parser')
-	all_links = soup.find_all('a')  # find all links
-	for link in all_links:
-		if(type(link.get('onclick'))==unicode):  #links to electives not pdfs
-			if(len(link.contents[0].contents)!=0):  #empty links that cannot be clicked
-				db[link.contents[0].contents[0]]=url_download + link.get('onclick')[14:len(link.get('onclick'))-6]
-		elif(type(link.get('onclick'))!=unicode):
-			ele_UG.append(url_dep + link.get('href'))
-
 #accumulating all PG courses for each Department
 for item in db_PG:
 	url = db_PG[item]
@@ -55,21 +35,12 @@ for item in db_PG:
 	soup = BeautifulSoup(response.text,'html.parser')
 	all_links = soup.find_all('a')  # find all links
 	for link in all_links:
-		if(type(link.get('onclick'))==unicode):  #links to electives not pdfs
+		if(type(link.get('onclick'))==unicode):  #links to depths not electives
 			if(len(link.contents[0].contents)!=0):  #empty links that cannot be clicked
 				db[link.contents[0].contents[0]]=url_download + link.get('onclick')[14:len(link.get('onclick'))-6]
 		elif(type(link.get('onclick'))!=unicode):
 			ele_PG.append(url_dep + link.get('href'))
 
-#accumulating all the UG Electives
-for item in ele_UG:
-	response = requests.get(item)
-	soup = BeautifulSoup(response.text,'html.parser')
-	all_links = soup.find_all('a')  # find all links
-	for link in all_links:
-		db_el[link.contents[0].contents[0]]=url_download + link.get('onclick')[14:len(link.get('onclick'))-6]
-
-#accumulating all the PG Electives
 for item in ele_PG:
 	response = requests.get(item)
 	soup = BeautifulSoup(response.text,'html.parser')
@@ -77,10 +48,6 @@ for item in ele_PG:
 	for link in all_links:
 		db_el[link.contents[0].contents[0]]=url_download + link.get('onclick')[14:len(link.get('onclick'))-6]
 
-
-#downloading all files
+# #downloading all files
 # for item in db:
-# 	download(db[item],item+'.pdf')
-
-# for item in db_el:
 # 	download(db[item],item+'.pdf')
