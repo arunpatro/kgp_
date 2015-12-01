77cbdf3fb55ef960e2d6bac97047db16d083861b
Arun Patro <arun.kumar.patro@gmail.com>
2015-12-01 19:05:16 +0530

    download UG and PG

--- fetch.py
+++ fetch.py
@@ -3,10 +3,12 @@ import requests
 from bs4 import BeautifulSoup
 
 url_UG = 'https://erp.iitkgp.ernet.in/ERPWebServices/curricula/specialisationList.jsp?stuType=UG'
+url_PG = 'https://erp.iitkgp.ernet.in/ERPWebServices/curricula/specialisationList.jsp?stuType=PG'
 url_dep = 'https://erp.iitkgp.ernet.in/ERPWebServices/curricula/'
 url_download = 'https://erp.iitkgp.ernet.in/ERPWebServices/curricula/commonFileDownloader.jsp?fileFullPath='
 
 db_UG={}
+db_PG={}
 db={}
 
 #to download one file
@@ -15,17 +17,35 @@ def download(url,name):
 	with open(name, "wb") as code:
 		code.write(f.read())
 
-#accumulating all curriculums for all Departments/Stream
+#accumulating all UG curriculums for all Departments/Stream
 response_UG = requests.get(url_UG)
 soup_UG = BeautifulSoup(response_UG.text,'html.parser')
 all_dep = soup_UG.find_all('a')
-db_UG={}
 for link in all_dep:
 	db_UG[link.get('href')[45:]]=url_dep + link.get('href')
 
-#accumulating all courses for each Department
+#accumulating all PG curriculums for all Departments/Stream
+response_PG = requests.get(url_PG)
+soup_PG = BeautifulSoup(response_PG.text,'html.parser')
+all_dep = soup_PG.find_all('a')
+for link in all_dep:
+	db_PG[link.get('href')[45:]]=url_dep + link.get('href')
+
+
+#accumulating all UG courses for each Department
 for item in db_UG:
 	url = db_UG[item]
+	response = requests.get(url)
+	soup = BeautifulSoup(response.text,'html.parser')
+	all_links = soup.find_all('a')  # find all links
+	for link in all_links:
+		if(type(link.get('onclick'))==unicode):  #links to electives not pdfs
+			if(len(link.contents[0].contents)!=0):  #empty links that cannot be clicked
+				db[link.contents[0].contents[0]]=url_download + link.get('onclick')[14:len(link.get('onclick'))-6]
+
+#accumulating all PG courses for each Department
+for item in db_PG:
+	url = db_PG[item]
 	response = requests.get(url)
 	soup = BeautifulSoup(response.text,'html.parser')
 	all_links = soup.find_all('a')  # find all links
@@ -38,5 +58,3 @@ for item in db_UG:
 #downloading all files
 for item in db:
 	download(db[item],item+'.pdf')
-
-
